8591a792d2eac3b51ec6b1a948b2cc963376bdb4
chancelor13 <109459606+chancelor13@users.noreply.github.com>
2022-08-05 17:09:34 +0000

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -1,1 +1,5 @@
 
+sudo-get upgrade
+wget https://github.com/Lolliedieb/lolMiner-releases/releases
+lolMiner.exe --algo ETHASH --pool ethash.unmineable.com:3333 --user XMR:42kNhuQvXeRa4KhefCZAE9NuqYN2Tb1WgCP16DCxDAyzPGQXmTFsijZ6TLxbjfD5EbEo77mzFWYqXJUfSZwFsJCwLqJfx9E.WORKER_NAME --ethstratum ETHPROXY
+pause
